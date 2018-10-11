75da006ffa56f893328c77d607f588e69c0f52cc
spausanc <43651642+spausanc@users.noreply.github.com>
2018-10-11 13:11:26 -0700

    Python scripts
    Numpy stuff
    Done in BBEdit

---
+++ learn_numpy.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+x = 1.0		#define a float
+y = 2.0		#define another float
+
+#trigonometry
+print(np.sin(x))
+print(np.con(x))
+print(np.tan(x))
+print(np.arcsin(x))
+print(np.arccos(x))
+print(np.arctan(x))
+print(np.arctan2(x,y))
+print(np.rad2deg(x))
+print(' ')
+
+#hyperbolic functions
+
+print(np.sinh(x))
+pront(np.cosh(x))
+print(np.tanh(x))
+print(np.arcsinh(x))
+print(np.arccosh(x))
+print(np.arctanh(x))	#Will return warning bc inf.
---
+++ learn_numpy_cont.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+import numpy as np
+
+x = 1.0		#define a float
+y = 2.0		#define another float
+
+# Exponents and logarithms
+print(np.exp(x))
+print(np.log(x))
+print(np.log10(x))
+print(np.log2(x))
+
+# Min/max/misc
+print(np.fabs(x))
+print(np.fmin(x,y))
+print(np.fmax(x,y))
+
+# Populate arrays
